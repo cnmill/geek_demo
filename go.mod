dc973bd6e7d95f1e5e0a6b970df22e03ead1d4fc
伯努 <bonu@ywwl.com>
2021-12-12 22:32:10 +0800

    提交作业

---
+++ go.mod
@@ -0,0 +1,13 @@
+module own/geek_demo
+
+go 1.17
+
+require (
+	github.com/pkg/errors v0.9.1
+	gorm.io/gorm v1.22.4
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
+)
---
+++ week2/mail.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+	"os"
+)
+
+/**
+* @Author: mill
+* @Date: 2021/12/9 7:09 下午
+ */
+
+//1. 我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
+// 答： 应该Warp异常，因为可以上层可以scan整个栈信息。
+func initDB() *gorm.DB {
+	dsn := "admin:admin@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+func main() {
+	var n string
+	n = "bonu"
+	u, err := dao(n)
+	if err != nil {
+		fmt.Println(errors.WithMessage(err, "not fount"))
+	} else {
+		fmt.Println(u)
+	}
+}
+
+type user struct {
+	id   uint
+	name string
+}
+
+func dao(n string) (u user, err error) {
+	db := initDB()
+	var u user
+	err := db.Raw("SELECT id,name"+
+		"FROM user").Where("name like ?", n).Scan(&u).Error
+	if err == sql.ErrNoRows {
+		return errors.Wrapf(errors.New("not found"), fmt.Sprintf("no user: %s", n))
+	}
+	if err != nil {
+		return errors.Wrapf(err)
+	}
+	return u, nil
+}
